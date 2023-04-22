196cb7c11bebed3931522e12f095ec305daabe11
Eric Njanga <eric.njanga@gmail.com>
2023-04-21 20:36:55 -0400

    Site renaming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'npdd-3'`, () => {
+  it(`should have as title 'Eric Njanga'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('npdd-3');
+    expect(app.title).toEqual('Eric Njanga');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('npdd-3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Eric Njanga app is running!');
   });
 });
