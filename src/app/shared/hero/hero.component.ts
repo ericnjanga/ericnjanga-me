218c5d3d869166dddb2035353490ca1ed9c3c87c
Eric Njanga <eric.njanga@gmail.com>
2023-04-25 12:18:01 -0400

    (Hero component) setting it up for multiple pages

--- src/app/shared/hero/hero.component.ts
+++ src/app/shared/hero/hero.component.ts
@@ -6,6 +6,33 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./hero.component.scss']
 })
 export class HeroComponent {
+  @Input() size: string = '';
   @Input() title: string = '';
   @Input() image: string = '';
+  @Input() page: string = '';
+
+
+  parentClassSetup() {
+    return `carousel--${this.size}`;
+    // return {
+    //   'carousel--small': this.size === 'small',
+    //   'carousel--large': this.size === 'large',
+    // };
+  }
+
+  slideClassSetup() {
+    return `slide--${this.page}`;
+  }
 }
+
+
+
+
+
+
+// imgClass(index: number) {
+//   return {
+//     'width1': index === 6,
+//     'width2': index === 9
+//   }
+// }
