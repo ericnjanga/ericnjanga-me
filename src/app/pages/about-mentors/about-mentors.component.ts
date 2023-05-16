12b9697774b594a17747ca2a741b24fc1fd8a4e4
Eric Njanga <eric.njanga@gmail.com>
2023-05-16 09:32:20 -0400

    about page responsiveness ...

--- src/app/pages/about-mentors/about-mentors.component.ts
+++ src/app/pages/about-mentors/about-mentors.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Testimonial } from 'src/app/projects/model/testimonial';
 
 @Component({
   selector: 'app-about-mentors',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AboutMentorsComponent {
 
+
+  ericQuote: Testimonial = 
+  {
+    name: 'Eric Njanga',
+    title: 'Their generous advice, nurturing feedback and honest critique gave me the direction, the challenge and the push my  certifications couldn’t.',
+    text: '...',
+    img: './../../assets/img/mentor 2.png',
+    linkedIn: '',
+  };
+
+
+
+  mentors: Testimonial[] = [
+    {
+      name: 'Dave Landis',
+      title: 'UX Design & Research + Innovation Architect [Design Thinking & Design Sprint Facilitation]',
+      text: '...',
+      img: './../../assets/img/mentor 1.png',
+      linkedIn: 'https://www.linkedin.com/in/dlandis/',
+    },
+    {
+      name: 'Alexandr Barsuk',
+      title: 'Head of User Experience. Previously @ Pacaso, Amazon Prime Video, SAP, XBOX, TiVo.',
+      text: '...',
+      img: './../../assets/img/mentor 2.png',
+      linkedIn: 'https://www.linkedin.com/in/alexbarsuk/',
+    },
+  ];
+
+
+  decorationNumber(index: number): number {
+    return index + 1;
+  }
 }
