a1aef075f13682604a20b2b3f8b5d45508e90ff7
Eric Njanga <eric.njanga@gmail.com>
2023-05-27 14:07:02 -0400

    Eliminating deleted component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,13 +21,13 @@ import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { SharedModule } from './shared/shared.module';
 import { ProjectsModule } from './projects/projects.module';
-import { RichyGroupComponent } from './pages-static/richy-group/richy-group.component';
 import { AboutDeveloperComponent } from './pages/about-developer/about-developer.component';
 import { AboutUxDesignerComponent } from './pages/about-ux-designer/about-ux-designer.component';
 import { AboutExcellenceComponent } from './pages/about-excellence/about-excellence.component';
 import { AboutProfessionalQualitiesComponent } from './pages/about-professional-qualities/about-professional-qualities.component';
 import { AboutMentorsComponent } from './pages/about-mentors/about-mentors.component';
 import { AboutTestimonialsComponent } from './pages/about-testimonials/about-testimonials.component';
+import { TypescaleComponent } from './pages/typescale/typescale.component';
 
 @NgModule({
   declarations: [
@@ -37,13 +37,13 @@ import { AboutTestimonialsComponent } from './pages/about-testimonials/about-tes
     LoginComponent,
     AboutComponent,
     PageNotFoundComponent,
-    RichyGroupComponent,
     AboutDeveloperComponent,
     AboutUxDesignerComponent,
     AboutExcellenceComponent,
     AboutProfessionalQualitiesComponent,
     AboutMentorsComponent,
     AboutTestimonialsComponent,
+    TypescaleComponent,
   ],
   imports: [
     BrowserModule,
