562e5b1c8c9bc7a41a06a574740e8695e95c0d0e
Eric Njanga <eric.njanga@gmail.com>
2023-07-04 14:26:57 -0400

    Case study 1 (phase 1)

--- src/app/case-studies/case-studies-routing.module.ts
+++ src/app/case-studies/case-studies-routing.module.ts
@@ -7,21 +7,7 @@ import { CaseStudy1Component } from './case-study1/case-study1.component';
 
 const routes: Routes = [
     { path: 'transformation-and-realignment-of-a-website-with-corporate-strategy', component: CaseStudy1Component },
-    // { path: '', component: HomeComponent },
-    // {
-    //     path: ':projectUrl',
-    //     component: ProjectComponent, 
-    //     /**
-    //      * Resolver will only load the component if data requested is available.
-    //      * (It helps prevent sooo many route-related issues|)
-    //      */
-    //     resolve: { // before fetching the component, first fetch the project property using ProjectResolver
-    //         project: ProjectResolver
-    //     }
-    // }
 ];
-
-// Transforming a corporate website to drive growth and align with strategy.
 
 
 @NgModule({
--- src/app/case-studies/case-studies.module.ts
+++ src/app/case-studies/case-studies.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CaseStudy1Component } from './case-study1/case-study1.component';
 import { CaseStudiesRoutingModule } from './case-studies-routing.module';
 
-
-
 @NgModule({
   declarations: [
     CaseStudy1Component
