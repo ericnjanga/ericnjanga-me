9dd86fe66a9da671a68c1ccb0d05abd55e592cad
Eric Njanga <eric.njanga@gmail.com>
2023-07-04 12:33:32 -0400

    First commit

--- src/app/shared/appbar/appbar.component.ts
+++ src/app/shared/appbar/appbar.component.ts
@@ -1,13 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2, ElementRef  } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-appbar',
   templateUrl: './appbar.component.html',
-  styleUrls: ['./appbar.component.scss']
+  styleUrls: ['./appbar.component.scss'],
+
+  // Aniamtion settings
+  animations: [
+    trigger('collapseAnimation', [
+      state('open', style({
+        height: '*',
+        visibility: 'visible',
+      })),
+      state('closed', style({
+        height: '0',
+        visibility: 'hidden',
+        // overflow: 'hidden',
+      })),
+      transition('open <=> closed', animate('250ms ease-in-out')),
+    ]),
+  ]
+
 })
 export class AppbarComponent {
   @Input() currentRoute!: string;
+  isCollapsed = true;
 
   // Navigate the user back to the previous route 
   goBack() {
@@ -24,6 +43,14 @@ export class AppbarComponent {
     return !!isChild[0];
   }
 
-  constructor(private location: Location) {
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+    // const navbarCollapse = this.elementRef.nativeElement.querySelector('.navbar-collapse');
+    // this.renderer.addClass(navbarCollapse, 'show');
+  }
+  
+
+  constructor(private location: Location, private renderer: Renderer2, private elementRef: ElementRef) {
+
   }
 }
