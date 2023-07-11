1b19075a52281a4e15eb0f2b8746113d09f66055
Eric Njanga <eric.njanga@gmail.com>
2023-07-10 22:46:09 -0400

    Gloabal layout responsiveness (phase 7)

--- src/app/case-studies/case-study1/case-study1.component.ts
+++ src/app/case-studies/case-study1/case-study1.component.ts
@@ -1,13 +1,31 @@
-import { Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import * as $ from 'jquery';
+import 'lightbox2';
+
+declare var lightbox: any;
 
 @Component({
   selector: 'app-case-study1',
   templateUrl: './case-study1.component.html',
   styleUrls: ['./case-study1.component.scss']
 })
-export class CaseStudy1Component {
+export class CaseStudy1Component implements AfterViewInit {
 
   constructor(private elementRef: ElementRef) {}
+
+
+  ngAfterViewInit(): void {
+    // https://lokeshdhakar.com/projects/lightbox2/#options
+    lightbox.option({
+      // disableScrolling: true,
+      fitImagesInViewport: false,
+      maxWidth: 600,
+      wrapAround: true
+      // Lightbox2 configuration options
+    });
+  }
+
+
 
   scrollToSection(sectionId: string) {
     const section = this.elementRef.nativeElement.querySelector(`#${sectionId}`);
@@ -19,6 +37,9 @@ export class CaseStudy1Component {
   ngOnInit() {
     // Set the scroll position to 0,0 when the component is initialized. 
     window.scrollTo(0, 0);
+
+
+  console.log('-------', $('h1'));
 
     // Set page title
     // this.titleService.setTitle('Eric Njanga | About');
