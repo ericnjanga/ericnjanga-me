7ac6f343595ee4793f4bc816c2b3d91c45dc0ea1
Eric Njanga <eric.njanga@gmail.com>
2023-07-13 09:34:18 -0400

    Preview deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './parts/footer/footer.component';
-import { LoginComponent } from './pages/login/login.component';
-import { AboutComponent } from './pages/about/about.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { SharedModule } from './shared/shared.module';
-import { ProjectsModule } from './projects/projects.module';
-import { AboutDeveloperComponent } from './pages/about-developer/about-developer.component';
-import { AboutUxDesignerComponent } from './pages/about-ux-designer/about-ux-designer.component';
-import { AboutExcellenceComponent } from './pages/about-excellence/about-excellence.component';
-import { AboutProfessionalQualitiesComponent } from './pages/about-professional-qualities/about-professional-qualities.component';
-import { AboutMentorsComponent } from './pages/about-mentors/about-mentors.component';
-import { AboutTestimonialsComponent } from './pages/about-testimonials/about-testimonials.component';
 import { TypescaleComponent } from './pages-static/typescale/typescale.component';
 import { CaseStudiesModule } from './case-studies/case-studies.module';
-// import { NgYoutubeEmbedModule } from 'ng-youtube-embed';
-
-// import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
-
 
 
 @NgModule({
@@ -31,15 +18,7 @@ import { CaseStudiesModule } from './case-studies/case-studies.module';
     AppComponent,
     HomeComponent,
     FooterComponent,
-    LoginComponent,
-    AboutComponent,
     PageNotFoundComponent,
-    AboutDeveloperComponent,
-    AboutUxDesignerComponent,
-    AboutExcellenceComponent,
-    AboutProfessionalQualitiesComponent,
-    AboutMentorsComponent,
-    AboutTestimonialsComponent,
     TypescaleComponent,
   ],
   imports: [
@@ -49,9 +28,7 @@ import { CaseStudiesModule } from './case-studies/case-studies.module';
     BrowserAnimationsModule,
     MatMenuModule,
     SharedModule,
-    ProjectsModule,
     CaseStudiesModule,
-    // ngYoutubeEmbed
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,56 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { IconComponent } from "./icon/icon.component";
-import { Test1Component } from './test1/test1.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ClientLogosComponent } from './client-logos/client-logos.component';
-import { TwoSkillsComponent } from './two-skills/two-skills.component';
-import { CallToActionComponent } from './call-to-action/call-to-action.component';
-import { LogoComponent } from './logo/logo.component';
-import { BoxPresentationComponent } from './box-presentation/box-presentation.component';
 import { MessagesComponent } from './messages/messages.component';
 import { LoadingComponent } from './loading/loading.component';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { TextTimPipe } from './pipes/text-trim/text-tim.pipe';
-import { CallToActionParamsComponent } from './call-to-action-params/call-to-action-params.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';  
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { TitleCaptionComponent } from './title-caption/title-caption.component';
-import { TestimonialComponent } from './testimonial/testimonial.component';
-import { QuoteComponent } from './quote/quote.component';
-import { NicePosterComponent } from './nice-poster/nice-poster.component';
+import { MatTooltipModule } from '@angular/material/tooltip'; 
 import { AppbarComponent } from './appbar/appbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { RevealContentComponent } from './reveal-content/reveal-content.component';
-import { HighlightSectionTextImgComponent } from './highlight-section-text-img/highlight-section-text-img.component';
+import { MatMenuModule } from '@angular/material/menu';  
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { FadeInSectionDirective } from './directives/fade-in-section.directive';
 
 @NgModule({
   declarations: [
-    IconComponent,
-    Test1Component,
-    CarouselComponent,
-    ClientLogosComponent,
-    TwoSkillsComponent,
-    CallToActionComponent,
-    LogoComponent,
-    BoxPresentationComponent,
     MessagesComponent,
     LoadingComponent,
-    TextTimPipe,
-    CallToActionParamsComponent,
-    TitleCaptionComponent,
-    TestimonialComponent,
-    QuoteComponent,
-    NicePosterComponent,
     AppbarComponent,
-    RevealContentComponent,
-    HighlightSectionTextImgComponent,
     FadeInSectionDirective,
   ],
   imports: [
@@ -66,23 +34,9 @@ import { FadeInSectionDirective } from './directives/fade-in-section.directive';
     MatBottomSheetModule,
   ],
   exports: [
-    IconComponent,
-    CarouselComponent,
-    ClientLogosComponent,
-    TwoSkillsComponent,
-    CallToActionComponent,
-    CallToActionParamsComponent,
-    LogoComponent,
     MessagesComponent,
     LoadingComponent,
-    TextTimPipe,
-    TitleCaptionComponent,
-    TestimonialComponent,
-    QuoteComponent,
-    NicePosterComponent,
     AppbarComponent,
-    RevealContentComponent,
-    HighlightSectionTextImgComponent,
     FadeInSectionDirective,
   ]
 })
